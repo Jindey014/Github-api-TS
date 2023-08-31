05fe4fa76ba0f3926b6e999ddbf8bbec6ae50968
Jinish Shrestha <dixon.cider014@gmail.com>
2023-09-01 00:55:14 +0545

    Final Commits

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const getUsername = document.querySelector("#user");
 const formSubmit = document.querySelector("#form");
 const main_container = document.querySelector(".main_container");
